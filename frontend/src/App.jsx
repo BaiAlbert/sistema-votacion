9071dc22885cc75b4aa9a7b535511b7c8fe21ff0
BaiAlbert <gameralbert105@gmail.com>
2026-01-24 01:35:05 +0100

    Maquetado simple inicial de la pagina de inicio. Creacion de la base de datos.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,65 +1,14 @@
-/*
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import './App.css';
+import { Header } from './Header.jsx';
+import { Body } from './Body.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
-*/
-import { useEffect, useState } from 'react';
-
-function App() {
-	const [usuarios, setUsuarios] = useState([]);
-
-	useEffect(() => {
-		// Aquí pedimos los datos a tu backend PHP
-		// Nota: Asumimos que PHP correrá en el puerto 8000
-		fetch('http://localhost:8000/api.php')
-			.then((response) => response.json())
-			.then((data) => setUsuarios(data))
-			.catch((error) => console.error('Error:', error));
-	}, []);
-
 	return (
-		<div style={{ padding: '20px' }}>
-			<h1>Lista de Usuarios desde PHP</h1>
-			<ul>
-				{usuarios.map((usuario) => (
-					<li key={usuario.id}>
-						<strong>{usuario.nombre}</strong> - {usuario.rol}
-					</li>
-				))}
-			</ul>
-		</div>
+		<React.Fragment>
+			<Header titulo="Sistema de Votación"></Header>
+			<Body />
+		</React.Fragment>
 	);
 }
 
